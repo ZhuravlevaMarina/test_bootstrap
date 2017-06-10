5656b0023f60521b787cce6512bb0d55890278b2
ZhuravlevaMarina <marinajuravleva28gmail.com>
2017-06-10 18:02:29 +0300

    animation hover icons

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -16,44 +16,46 @@ define(["jquery", "bootstrap", "domReady"], function($) {
             $(window).on('load', function(){
                 initTeamTabs('#team');
             });
-
-            function initTeamTabs(id) {
-                var initialActiveElement = $(id).find('a.team-member.active');
-                handleToggle(initialActiveElement);
-
-                // Handle click
-                $(id).find('a.team-member').each(function(){
-                    $(this).on('click', function(e){
-                        e.preventDefault();
-                        handleToggle($(this));
-                    })
-                });
-
-                // Handle Close
-                $(id).find('a.close').each(function(){
-                    $(this).on('click', function(e){
-                        e.preventDefault();
-                        $(id).find('div.personal-box.active').hide(250).removeClass('active');
-                        $(id).find('a.team-member').each(function(){
-                            $(this).removeClass('active');
-                        });
-                    })
-                });
-
-                // Toggle function
-                function handleToggle(element){
-                    var itemId = element.attr('data-id');
-                    $(id).find('div.personal-box.active').hide(250).removeClass('active');
-                    $(id).find('a.team-member').each(function(){
-                        $(this).removeClass('active');
-                    });
-                    element.addClass('active');
-                    $(id).find('#'+itemId).addClass('active').toggle(500);
-                }
-            }
         }
     };
 
+    /* Init Home Page Scripts */
     Homepage.init();
+
+    /* Team Tabs Function */
+    function initTeamTabs(id) {
+        var initialActiveElement = $(id).find('a.team-member.active');
+        handleToggle(initialActiveElement);
+
+        // Handle click
+        $(id).find('a.team-member').each(function(){
+            $(this).on('click', function(e){
+                e.preventDefault();
+                handleToggle($(this));
+            })
+        });
+
+        // Handle Close
+        $(id).find('a.close').each(function(){
+            $(this).on('click', function(e){
+                e.preventDefault();
+                $(id).find('div.personal-box.active').hide(250).removeClass('active');
+                $(id).find('a.team-member').each(function(){
+                    $(this).removeClass('active');
+                });
+            })
+        });
+
+        // Toggle function
+        function handleToggle(element){
+            var itemId = element.attr('data-id');
+            $(id).find('div.personal-box.active').hide(250).removeClass('active');
+            $(id).find('a.team-member').each(function(){
+                $(this).removeClass('active');
+            });
+            element.addClass('active');
+            $(id).find('#'+itemId).addClass('active').toggle(500);
+        }
+    }
 
 });
